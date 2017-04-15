4def3ee0765beb1c12ae5d17102791ad9e6adf72
jayanp@gmail.com <jayanp@gmail.com>
2017-04-15 12:09:24 -0400

    Add project files.

---
+++ GetRepo3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetRepo3
+{
+    class Program
+    {
+        /// <summary>
+        ///  Test
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("test1");
+            Console.ReadLine();
+        }
+    }
+}
